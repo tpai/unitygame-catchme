370db816497bfbabd8969b617e47e5e84d3dd467
tonypai <tony77794@gmail.com>
2015-03-22 14:33:04 +0800

    動態生怪、製作死穴

---
+++ Assets/Scripts/Deadpit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deadpit : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Enemy") {
+			Destroy (target.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner: MonoBehaviour {
+	
+	public GameObject prefab;
+	
+	void Start() {
+		InvokeRepeating ("SpawnEnemy", .1f, 1f);
+	}
+	
+	void SpawnEnemy() {
+		GameObject obj = (GameObject) Instantiate(prefab);
+		obj.name = "Enemy";
+		obj.transform.position = new Vector2 (
+			Random.Range (-22f, 22f),
+			17f
+		);
+	}
+}
