098e490fdbbd31e1a88c670ff67e14bca91c50c0
tonypai <tony77794@gmail.com>
2015-03-29 10:18:58 +0800

    分數加減分

--- Assets/Scripts/PlayerContacts.cs
+++ Assets/Scripts/PlayerContacts.cs
@@ -4,7 +4,25 @@ using System.Collections;
 public class PlayerContacts : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D coll) {
-		if (coll.collider.tag == "Enemy") {
+//		if (coll.collider.tag == "Enemy") {
+//			Score.AddScore(-5);
+//			Destroy (coll.collider.gameObject);
+//		}
+//
+//		if (coll.collider.tag == "Friendly") {
+//			Score.AddScore(15);
+//			Destroy (coll.collider.gameObject);
+//		}
+
+		string tag = coll.collider.tag;
+		if (
+			tag == "Enemy" ||
+			tag == "Friendly"
+		) {
+			switch(tag) {
+				case "Enemy": Score.AddScore(-5); break;
+				case "Friendly": Score.AddScore(15); break;
+			}
 			Destroy (coll.collider.gameObject);
 		}
 	}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public static int nowScore = 0;
+
+	void Update () {
+		GetComponent<Text> ().text = "Score: " + nowScore.ToString ();
+	}
+
+	public static void AddScore (int amt) {
+		nowScore += amt;
+	}
+}
