c7c4d4f95f2d406de2054746d0a50eb1af86ab4e
tonypai <tony77794@gmail.com>
2015-03-29 14:49:21 +0800

    將前三名分數以及現在分數存入陣列後做小到大排序再反轉後寫回PlayerPrefs

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -4,10 +4,34 @@ using System.Collections;
 public class Leaderboard : MonoBehaviour {
 
 	int nowScore;
+	int[] scoreList;
 
 	void Start () {
+		// define
+		scoreList = new int[4];
 		nowScore = PlayerPrefs.GetInt ("NowScore");
-		Debug.Log (nowScore);
+
+		// top3 insert into array
+		for (int i=1; i<=3; i++) {
+			int score = PlayerPrefs.GetInt("NO"+i);
+			scoreList[i-1] = score;
+		}
+		// nowscore insert into array
+		scoreList [3] = nowScore;
+
+		// sort and reverse
+		System.Array.Sort (scoreList);
+		System.Array.Reverse (scoreList);
+
+		// write back to playerprefs
+		for (int i=1; i<4; i++) {
+			PlayerPrefs.SetInt("NO"+i, scoreList[i-1]);
+		}
+
+		// print
+		for (int i=1; i<4; i++) {
+			Debug.Log (PlayerPrefs.GetInt("NO"+i));
+		}
 	}
 
 	void Update () {
